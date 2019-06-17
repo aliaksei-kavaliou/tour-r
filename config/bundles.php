65e8572358133660bffd5b8cfb8b017c2a41e5f2
Aliaksei Kavaliou <aliaksei.kavaliou@geniussports.com>
2019-06-17 09:02:44 +0300

    messenger fix

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,7 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Aws\Symfony\AwsBundle::class => ['all' => true],
+    Enqueue\Bundle\EnqueueBundle::class => ['all' => true],
+    Enqueue\MessengerAdapter\Bundle\EnqueueAdapterBundle::class => ['all' => true],
 ];
