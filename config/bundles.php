c58c87b1408c28f1c7867701034658d4790d9e49
Aliaksei Kavaliou <aliaksei.kavaliou@geniussports.com>
2019-06-17 19:25:23 +0300

    remove

--- config/bundles.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Aws\Symfony\AwsBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-];
